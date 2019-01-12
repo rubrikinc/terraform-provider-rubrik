7d4267a70d03179c63795acb7a17326966d2d98d
Drew Russell <Drew.Russell@rubrik.com>
2019-01-11 19:43:40 -0600

    update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-package=$1
-if [[ -z "$package" ]]; then
-    echo "usage: $0 <package-name>"
-    exit 1
-fi
 package_split=(${package//\// })
 package_name="terraform-provider-rubrik"
 
