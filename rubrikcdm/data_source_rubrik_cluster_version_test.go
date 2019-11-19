f7cb9b514d6f167b98ca7cb3e5b29db6387ee710
Matt Elliott <mattadamelliott@gmail.com>
2019-11-18 20:51:13 -0500

    Update env variable reference

--- rubrikcdm/data_source_rubrik_cluster_version_test.go
+++ rubrikcdm/data_source_rubrik_cluster_version_test.go
@@ -12,14 +12,14 @@ func TestAccDataSourceRubrikClusterVersion_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
-			testAccCheckEnvVariables(t, []string{"rubrik_cdm_expected_version"})
+			testAccCheckEnvVariables(t, []string{"RUBRIK_CDM_EXPECTED_VERSION"})
 		},
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccDataRubrikClusterVersion(),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.rubrik_cluster_version.version", "cluster_version", os.Getenv("rubrik_cdm_expected_version")),
+					resource.TestCheckResourceAttr("data.rubrik_cluster_version.version", "cluster_version", os.Getenv("RUBRIK_CDM_EXPECTED_VERSION")),
 				),
 			},
 		},
