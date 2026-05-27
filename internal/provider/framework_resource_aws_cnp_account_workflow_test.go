003f4933a8a4fca722e34f87c3815c2e0c91129d
Johan Andersson <johan.andersson@rubrik.com>
2026-05-27 10:51:04 +0200

    Add acceptance test for the AWS IAM-roles onboarding workflow
    End-to-end test that wires rubrik_aws_cnp_artifacts and
    rubrik_aws_cnp_permissions to the rubrik_aws_cnp_account and
    rubrik_aws_cnp_account_attachments resources via real aws_iam_role,
    aws_iam_policy and aws_iam_role_policy_attachment(s_exclusive)
    resources, covering a single CROSSACCOUNT role under CLOUD_DISCOVERY
    and CLOUD_NATIVE_PROTECTION.

---
+++ internal/provider/framework_resource_aws_cnp_account_workflow_test.go
@@ -0,0 +1,176 @@
+// Copyright 2026 Rubrik, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/compare"
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+// TestAccAwsCnpAccountWorkflow exercises the full AWS IAM-roles onboarding
+// workflow end-to-end.
+func TestAccAwsCnpAccountWorkflow(t *testing.T) {
+	account, err := loadAWSTestConf()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: protoV6ProviderFactories,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"aws": {
+				Source:            "hashicorp/aws",
+				VersionConstraint: ">=6.0.0",
+			},
+		},
+		CheckDestroy: resource.ComposeAggregateTestCheckFunc(
+			awsCnpAccountCheckDestroy(t.Context()),
+			awsCnpAccountAttachmentsCheckDestroy(t.Context()),
+		),
+		Steps: []resource.TestStep{{
+			// Note, the Terraform Plugin Testing package does not support
+			// for_each on data source or resources, only count.
+			Config: `
+				variable "account_name" {
+					type = string
+				}
+				variable "aws_account_id" {
+					type = string
+				}
+
+				data "polaris_aws_cnp_artifacts" "artifacts" {
+					feature {
+						name              = "CLOUD_DISCOVERY"
+						permission_groups = ["BASIC"]
+					}
+					feature {
+						name              = "CLOUD_NATIVE_PROTECTION"
+						permission_groups = ["BASIC"]
+					}
+				}
+
+				data "polaris_aws_cnp_permissions" "crossaccount" {
+					role_key = "CROSSACCOUNT"
+
+					dynamic "feature" {
+						for_each = data.polaris_aws_cnp_artifacts.artifacts.feature
+						content {
+							name              = feature.value["name"]
+							permission_groups = feature.value["permission_groups"]
+						}
+					}
+				}
+
+				resource "polaris_aws_cnp_account" "account" {
+					name      = var.account_name
+					native_id = var.aws_account_id
+					regions   = ["us-east-2"]
+
+					dynamic "feature" {
+						for_each = data.polaris_aws_cnp_artifacts.artifacts.feature
+						content {
+							name              = feature.value["name"]
+							permission_groups = feature.value["permission_groups"]
+						}
+					}
+				}
+
+				resource "aws_iam_role" "crossaccount" {
+					name_prefix = "tfacc-crossaccount-"
+
+					assume_role_policy = one([
+						for p in polaris_aws_cnp_account.account.trust_policies : p.policy if p.role_key == "CROSSACCOUNT"
+					])
+				}
+
+				resource "aws_iam_policy" "crossaccount" {
+					count       = length(data.polaris_aws_cnp_permissions.crossaccount.customer_managed_policies)
+					name_prefix = "tfacc-crossaccount-${data.polaris_aws_cnp_permissions.crossaccount.customer_managed_policies[count.index].name}-"
+					policy      = data.polaris_aws_cnp_permissions.crossaccount.customer_managed_policies[count.index].policy
+				}
+
+				resource "aws_iam_role_policy_attachment" "crossaccount" {
+					count      = length(aws_iam_policy.crossaccount)
+					role       = aws_iam_role.crossaccount.name
+					policy_arn = aws_iam_policy.crossaccount[count.index].arn
+				}
+
+				resource "aws_iam_role_policy_attachments_exclusive" "crossaccount" {
+					role_name   = aws_iam_role.crossaccount.name
+					policy_arns = concat(data.polaris_aws_cnp_permissions.crossaccount.managed_policies, aws_iam_policy.crossaccount[*].arn)
+				}
+
+				resource "polaris_aws_cnp_account_attachments" "attachments" {
+					account_id = polaris_aws_cnp_account.account.id
+					features   = data.polaris_aws_cnp_artifacts.artifacts.feature.*.name
+
+					role {
+						key         = "CROSSACCOUNT"
+						arn         = aws_iam_role.crossaccount.arn
+						permissions = data.polaris_aws_cnp_permissions.crossaccount.id
+					}
+				}
+			`,
+			ConfigVariables: config.Variables{
+				"account_name":   config.StringVariable(account.AccountName),
+				"aws_account_id": config.StringVariable(account.AccountID),
+			},
+			ConfigStateChecks: []statecheck.StateCheck{
+				statecheck.ExpectKnownValue("data.polaris_aws_cnp_artifacts.artifacts",
+					tfjsonpath.New(keyRoleKeys),
+					knownvalue.SetExact([]knownvalue.Check{
+						knownvalue.StringExact("CROSSACCOUNT"),
+					})),
+				statecheck.ExpectKnownValue("data.polaris_aws_cnp_artifacts.artifacts",
+					tfjsonpath.New(keyInstanceProfileKeys),
+					knownvalue.SetExact([]knownvalue.Check{})),
+				statecheck.ExpectKnownValue("polaris_aws_cnp_account.account",
+					tfjsonpath.New(keyID), NonNullUUID()),
+				statecheck.ExpectKnownValue("polaris_aws_cnp_account.account",
+					tfjsonpath.New(keyName), knownvalue.StringExact(account.AccountName)),
+				statecheck.CompareValuePairs(
+					"polaris_aws_cnp_account.account", tfjsonpath.New(keyID),
+					"polaris_aws_cnp_account_attachments.attachments", tfjsonpath.New(keyID),
+					compare.ValuesSame()),
+				statecheck.CompareValuePairs(
+					"polaris_aws_cnp_account.account", tfjsonpath.New(keyID),
+					"polaris_aws_cnp_account_attachments.attachments", tfjsonpath.New(keyAccountID),
+					compare.ValuesSame()),
+				statecheck.ExpectKnownValue("polaris_aws_cnp_account_attachments.attachments",
+					tfjsonpath.New(keyFeatures),
+					knownvalue.SetExact([]knownvalue.Check{
+						knownvalue.StringExact("CLOUD_DISCOVERY"),
+						knownvalue.StringExact("CLOUD_NATIVE_PROTECTION"),
+					})),
+				statecheck.CompareValuePairs(
+					"aws_iam_role.crossaccount", tfjsonpath.New(keyARN),
+					"polaris_aws_cnp_account_attachments.attachments",
+					tfjsonpath.New(keyRole).AtSliceIndex(0).AtMapKey(keyARN), compare.ValuesSame()),
+			},
+		}},
+	})
+}
